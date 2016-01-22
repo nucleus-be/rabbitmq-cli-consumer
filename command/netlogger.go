741267866b91e406c42c3e29851469f1c18dac16
Dries Pauwels <dries@nucleus.be>
2016-01-22 15:09:58 +0100

    added required rpc fields/structure

--- command/netlogger.go
+++ command/netlogger.go
@@ -3,13 +3,25 @@ package command
 import (
 	"bytes"
 	"encoding/json"
+	"math/rand"
 	"net/http"
 )
 
 type ProvisionEvent struct {
+	JsonRpc string      `json:"jsonrpc"`
+	Method  string      `json:"method"`
+	Id      int32       `json:"id"`
+	Params  *Parameters `json:"params"`
+}
+
+type Data struct {
 	Error   bool   `json:"is_error"`
 	Output  string `json:"out"`
 	Message []byte `json:"message"`
+}
+
+type Parameters struct {
+	Data *Data `json:"data"`
 }
 
 type NetLogger struct {
@@ -18,18 +30,32 @@ type NetLogger struct {
 
 func (n *NetLogger) SendOK(p []byte, bod []byte) error {
 	event := ProvisionEvent{
-		Error:   false,
-		Output:  string(p),
-		Message: bod,
+		JsonRpc: "2.0",
+		Method:  "Event::createProvisioningEvent",
+		Id:      rand.Int31(),
+		Params: &Parameters{
+			Data: &Data{
+				Error:   false,
+				Output:  string(p),
+				Message: bod,
+			},
+		},
 	}
 	return n.send(&event)
 }
 
 func (n *NetLogger) SendError(p []byte, bod []byte) error {
 	event := ProvisionEvent{
-		Error:   true,
-		Output:  string(p),
-		Message: bod,
+		JsonRpc: "2.0",
+		Method:  "Event::createProvisioningEvent",
+		Id:      rand.Int31(),
+		Params: &Parameters{
+			Data: &Data{
+				Error:   true,
+				Output:  string(p),
+				Message: bod,
+			},
+		},
 	}
 	return n.send(&event)
 }
